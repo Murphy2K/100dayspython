9452cac7d24d6090aa6a17fc3c4ea4768c152678
Heiko <parrot@localhost.localdomain>
2023-06-26 13:25:44 +0300

    solve  day 1 exercise

--- Day1Exercise.py
+++ Day1Exercise.py
@@ -1,10 +1,12 @@
 #1. Create a greeting for your program.
+print("Tere tulemast!")
 
 #2. Ask the user for the city that they grew up in.
+linn = input("Mis linnas sina syndisid?\n")
 
 #3. Ask the user for the name of a pet.
+loom = input("Mis on sinu lemmiklooma nimi?\n")
 
 #4. Combine the name of their city and pet and show them their band name.
-
+print ("Sinu rockp2ndi nimi on", linn, loom)
 #5. Make sure the input cursor shows on a new line:
-
