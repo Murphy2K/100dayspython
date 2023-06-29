5ac2476e4ca6f40e88e58b5c4be543d7e4062489
Heiko <parrot@localhost.localdomain>
2023-06-29 13:03:04 +0300

    Day 2 exercise finish

--- Day2Exersise.py
+++ Day2Exersise.py
@@ -11,7 +11,7 @@
 
 print("Welcome to the tip calculator.")
 bill = float(input("What was the total bill? "))
-percentage = int(input("What percentage tip would you like to give? 10, 20 or 15? "))
+percentage = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
 bill_percentage = (percentage/100)*bill
 total_bill = bill_percentage + bill
