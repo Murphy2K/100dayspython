5422e47317cb9ab85a644a94b0e02c4ce7bccc6a
Heiko <parrot@localhost.localdomain>
2023-06-29 13:16:10 +0300

    Day 3 exercise start

--- Day3Exercise.py
+++ Day3Exercise.py
@@ -27,3 +27,16 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
+'''
+### Text Snippets from my example
+
+* 'You\'re at a crossroad. Where do you want to go? Type "left" or "right"'
+* 'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.'
+* "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?"
+* "It\'s a room full of fire. Game Over."
+* "You found the treasure! You Win!"
+* "You enter a room of beasts. Game Over."
+* "You chose a door that doesn\'t exist. Game Over."
+* "You get attacked by an angry trout. Game Over."
+* "You fell into a hole. Game Over."
+'''
