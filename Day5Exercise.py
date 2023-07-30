afda8aeb35224a7a237e3fd979ae0fa97130ee42
Heiko <parrot@localhost.localdomain>
2023-07-30 12:54:17 +0300

    Finish password generator Day5 exercise

--- Day5Exercise.py
+++ Day5Exercise.py
@@ -5,13 +5,41 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+total= []
+for i in range(nr_letters):
+    random_item = random.choice(letters)
+    total.append(random_item)
+
+
+for i in range(nr_symbols):
+    random_item = random.choice(symbols)
+    total.append(random_item)
+
+
+for i in range(nr_numbers):
+    random_item = random.choice(numbers)
+    total.append(random_item)
+
+print("The password is:", total)
+
+
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+final = []
+total_length = int(len(total))
+
+for i in range(total_length):
+    random_item = random.choice(total)
+    final.append(random_item)
+
+final_password = "".join(final) # Convert the final list into a string
+
+print("Final password is: ", final_password)
